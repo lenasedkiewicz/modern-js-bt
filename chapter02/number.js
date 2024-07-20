9e0a4ca1c083544b9bef8444899d5abec44c8e2a
msedkiewicz <magdalenainspirations@gmail.com>
2024-07-20 11:21:50 +0200

    Number challenge

---
+++ chapter02/number.js
@@ -0,0 +1,37 @@
+const x = Math.floor(Math.random() * 100 + 1);
+const y = Math.floor(Math.random() * 50 + 1);
+
+// Sum
+const sum = x + y;
+const sumOutput = `${x} + ${y} = ${sum}`;
+
+console.log(sum);
+console.log(sumOutput);
+
+// Difference
+const diff = x - y;
+const diffOutput = `${x} - ${y} = ${diff}`;
+
+console.log(diff);
+console.log(diffOutput);
+
+// Product
+const prod = x * y;
+const prodOutput = `${x} * ${y} = ${prod}`;
+
+console.log(prod);
+console.log(prodOutput);
+
+// Quotient
+const quot = x / y;
+const quotOutput = `${x} / ${y} = ${quot}`;
+
+console.log(quot);
+console.log(quotOutput);
+
+// Remainder
+const rem = x % y;
+const remOutput = `${x} % ${y} = ${rem}`;
+
+console.log(rem);
+console.log(remOutput);
