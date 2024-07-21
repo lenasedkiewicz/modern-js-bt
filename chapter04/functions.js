4203345fbfdacc1ef28a05c9201953dcdb552cc4
msedkiewicz <magdalenainspirations@gmail.com>
2024-07-21 21:37:29 +0200

    Comparison function

--- chapter04/functions.js
+++ chapter04/functions.js
@@ -5,8 +5,21 @@ function getCelcius(F) {
   return celsius;
 }
 
-console.log(getCelcius(32));
-console.log(getCelcius(100));
+// console.log(getCelcius(32));
+// console.log(getCelcius(100));
 
 const getCelciusArrow = (F) => ((F - 32) * 5) / 9;
-console.log(getCelciusArrow(150));
+// console.log(getCelciusArrow(150));
+console.log(`The temp outside is ${getCelciusArrow(140)}`);
+
+// Return min and max values in an array
+function minMax(arr) {
+  const min = Math.min(...arr);
+  const max = Math.max(...arr);
+  return {
+    min,
+    max,
+  };
+}
+
+console.log(minMax([1, 2, 3, 4, 5, 6, 7, 100]));
