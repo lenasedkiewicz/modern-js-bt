f1f073bc7058c1ecbefebdfcdcda33af39b66a6b
msedkiewicz <magdalenainspirations@gmail.com>
2025-03-24 16:08:14 +0100

    Add icon to button

--- shopping-list/script.js
+++ shopping-list/script.js
@@ -23,7 +23,15 @@ function addItem(e) {
 function createButton(classes) {
   const button = document.createElement("button");
   button.className = classes;
+  const icon = createIcon("fa-solid fa-xmark");
+  button.appendChild(icon);
   return button;
+}
+
+function createIcon(classes) {
+  const icon = document.createElement("i");
+  icon.className = classes;
+  return icon;
 }
 
 // Event listeners - keeping them on the bottom
