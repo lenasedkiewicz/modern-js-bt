becaf7769004abaa25d204a515ebcc0029114b08
msedkiewicz <magdalenainspirations@gmail.com>
2025-03-25 13:35:34 +0100

    Add proper object removal; add all items clearing

--- shopping-list/script.js
+++ shopping-list/script.js
@@ -1,6 +1,7 @@
 const itemForm = document.getElementById("item-form");
 const itemInput = document.getElementById("item-input");
 const itemList = document.getElementById("item-list");
+const clearBtn = document.getElementById("clear");
 
 function addItem(e) {
   e.preventDefault();
@@ -39,10 +40,17 @@ function createIcon(classes) {
 
 function removeItem(e) {
   if (e.target.parentElement.classList.contains("remove-item")) {
-    console.info("click");
+    e.target.parentElement.parentElement.remove();
+  }
+}
+
+function clearItems() {
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
   }
 }
 
 // Event listeners - keeping them on the bottom
 itemForm.addEventListener("submit", addItem);
 itemList.addEventListener("click", removeItem);
+clearBtn.addEventListener("click", clearItems);
