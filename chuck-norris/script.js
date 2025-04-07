bc8e47d5a87c984c6f9241e28c28e5c0ff79eed8
msedkiewicz <magdalenainspirations@gmail.com>
2025-04-07 12:39:45 +0200

    Code refactoring

--- chuck-norris/script.js
+++ chuck-norris/script.js
@@ -1,21 +1,24 @@
-const joke = document.getElementById("joke");
+const jokeEl = document.getElementById("joke");
 const jokeBtn = document.getElementById("joke-btn");
 
-function getJoke() {
-  joke.innerHTML = "Loading";
+jokeBtn.addEventListener("click", generateJoke);
+
+function generateJoke() {
   const xhr = new XMLHttpRequest();
   xhr.open("GET", "https://api.chucknorris.io/jokes/random");
 
   xhr.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
-      const data = JSON.parse(this.responseText);
-      joke.innerHTML = data.value;
+    if (this.readyState === 4) {
+      if (this.status === 200) {
+        jokeEl.innerHTML = JSON.parse(this.responseText).value;
+      } else {
+        jokeEl.innerHTML = "Something went wrong(Not Funny)";
+      }
     }
   };
 
   xhr.send();
 }
 
-getJoke();
-
-jokeBtn.addEventListener("click", getJoke);
+jokeBtn.addEventListener("click", generateJoke);
+document.addEventListener("DOMContentLoaded", generateJoke);
