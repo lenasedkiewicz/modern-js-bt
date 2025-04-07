fba6473c1fb0d2663aaff17b94da152dc631931c
msedkiewicz <magdalenainspirations@gmail.com>
2025-04-07 12:16:10 +0200

    Fetch repository info - notes

--- asynchronousJS/ajax.js
+++ asynchronousJS/ajax.js
@@ -1,6 +1,7 @@
 const xhr = new XMLHttpRequest();
 
-xhr.open("GET", "./ajax.json");
+// xhr.open("GET", "./ajax.json");
+xhr.open("GET", "https://api.github.com/users/lenasedkiewicz/repos");
 
 // readyState has 5 possible values
 // - 0: request not initialized
@@ -16,9 +17,9 @@ xhr.onreadystatechange = function () {
   if (this.readyState === 4 && this.status === 200) {
     // console.log(JSON.parse(this.responseText));
     const data = JSON.parse(this.responseText);
-    data.forEach((movie) => {
+    data.forEach((repo) => {
       const li = document.createElement("li");
-      li.innerHTML = `<strong>${movie.title}</strong> - ${movie.year}`;
+      li.innerHTML = `<strong>${repo.name}</strong> - ${repo.description}`;
       document.querySelector("#results").appendChild(li);
     });
   }
