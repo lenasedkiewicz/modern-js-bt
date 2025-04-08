ba0bd8384921940fa9b249608c8550f6f1b1acd2
msedkiewicz <magdalenainspirations@gmail.com>
2025-04-08 18:26:59 +0200

    add finally to promise

--- asynchronousJS/promises.js
+++ asynchronousJS/promises.js
@@ -3,7 +3,7 @@
 const getUser = new Promise((resolve, reject) => {
   // some async task
   setTimeout(() => {
-    let error = true;
+    let error = false;
 
     if (!error) {
       resolve({ name: "John", age: 34 });
@@ -19,6 +19,9 @@ getUser
   })
   .catch((error) => {
     console.error(error);
-  });
+  })
+  .finally(() => {
+    console.info("The promise has been resolved OR rejected");
+  }); // will run no matter if promise was resolved or not
 
 console.info("Hello from global scope");
