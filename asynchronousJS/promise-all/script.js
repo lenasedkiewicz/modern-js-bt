e7ed821bdba344ffbbebeae7ad6d9de16348e76d
msedkiewicz <magdalenainspirations@gmail.com>
2025-04-08 19:16:47 +0200

    Use Promise.all()

--- asynchronousJS/promise-all/script.js
+++ asynchronousJS/promise-all/script.js
@@ -20,16 +20,12 @@ function getData(endpoint) {
   });
 }
 
-getData("./movies.json")
-  .then((movies) => {
-    console.log(movies);
-    return getData("./actors.json");
-  })
-  .then((actors) => {
-    console.log(actors);
-    return getData("./directors.json");
-  })
-  .then((directors) => {
-    console.log(directors);
+const moviesPromise = getData("./movies.json");
+const actorsPromise = getData("./actors.json");
+const directorsPromise = getData("./directors.json");
+
+Promise.all([moviesPromise, actorsPromise, directorsPromise])
+  .then((data) => {
+    console.log(data);
   })
   .catch((error) => console.error(error));
